d472e8cd96c4e98f5c715496818f9235d6018f89
spaceyb0t <elviracbarrientos@gmail.com>
2018-05-11 22:44:15 -0500

    it's finally working and looking nice

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -4,8 +4,57 @@ $(document).ready(function(){
 //GLOBAL VARIABLES 
 //====================================================================
 
-var movieCategories = ["horror", "sci-fi", "comedy", "drama", "documentaries"];
+var movieCategories = ["monday", "sci-fi", "breakup", "new years", "dancing"];
 console.log(movieCategories)
+
+
+//FUNCTIONS
+//=====================================================================
+function displayMovieInfo()
+{
+    $("#gifHome").empty();
+//API key 
+var movie = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=AMEh4BzCNmxQIQdfRl5K60PbvU7r4QFn&limit=10";
+//Make AJAX call to Giphy API 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then (function(response){
+    var results = response.data
+    console.log(results)
+
+{
+
+// //For loop for categories
+   for(var i = 0; i < 10; i++ )
+   {
+//  // //Create a div for each gif
+  var theMovie = $("<div>")
+//  // //Add class to theMovie
+   theMovie.addClass("theMovieClass")
+//  //Var for rating
+   var rating = results[i].rating;
+//  //paragraph with rating
+   var ratingInfo = $("<p>").text("Rating:" + rating);
+//  //Create image info
+   var gifPic = $("<img>");
+//  //Give image tag src to show image
+   gifPic.attr("src", results[i].images.fixed_height_still.url);
+//  //Give gif data-still attribute
+   gifPic.attr("data-still", results[i].images.fixed_height_still.url);
+   gifPic.attr("data-animate", results[i].images.fixed_height.url);
+   gifPic.attr("data-state", "still");
+   gifPic.addClass("gifPic");
+//   //Append ratingInfo and gifPic to theMovie
+   theMovie.append(ratingInfo);
+   theMovie.append(gifPic);
+//  //Append theMovie to html
+   $("#gifHome").prepend(theMovie);
+   }
+}
+    })
+}
 
 function theButtons()
 {
@@ -27,84 +76,44 @@ function theButtons()
     }
 }
 
-//FUNCTIONS
-//=====================================================================
-function displayMovieInfo()
-{
-//API key 
-var movie = $(this).attr("data-name");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=AMEh4BzCNmxQIQdfRl5K60PbvU7r4QFn&limit=10";
-//Make AJAX call to Giphy API 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then (function(response){
-    var results = response.data
-    console.log(results)
-
-{
-
-// //For loop for categories
-   for(var i = 0; i < results.length; i++ )
-   {
-//  // //Create a div for each gif
-  var theMovie = $("<div>")
-//  // //Add class to theMovie
-   theMovie.addClass("theMovieClass")
-//  //Var for rating
-   var rating = results[i].rating;
-//  //paragraph with rating
-   var ratingInfo = $("<p>").text("Rating:" + rating);
-//  //Create image info
-   var gifPic = $("<img>");
-//  //Give image tag src to show image
-   gifPic.attr("src", results[i].images.fixed_height_still.url);
-//  //Give gif data-still attribute
-   gifPic.attr("data-still", results[i].images.fixed_height.url);
-   gifPic.attr("data-state", "still");
-   gifPic.addClass("gifPic");
-//   //Append ratingInfo and gifPic to theMovie
-   theMovie.append(ratingInfo);
-   theMovie.append(gifPic);
-//  //Append theMovie to html
-   $("#gifHome").prepend(theMovie);
-   }
-}
-    })
-}
-
  $(document).on("click", ".gifButton", displayMovieInfo);
  //Add gifs when theButtons are clicked 
  $("#addGif").on("click", function(event)
  {
      event.preventDefault();
-    //Grab input from html
-    var gif = $("#gifInput").val().trim();
-   //Add cartoon to gifArray
-    movieCategories.push(gif);
-    //Empty value in input box
+     
+     //Grab input from html
+     var gif = $("#gifInput").val().trim();
+     //Add cartoon to gifArray
+     movieCategories.push(gif);
+     //Empty value in input box
      $("#gifInput").val("")
-    //Call theButtons function to update new button
+     //Call theButtons function to update new button
      theButtons();
- })
-
+});
+    
 $("body").on("click", ".gifPic", function(){
     var state = $(this).attr("data-state");
     //if the pic is still
     if(state === "still")
-{
-    $(this).attr("src", $(this).attr("data-animate"));
-    //change to animate
-    $(this).attr("data-state", "animate");
-}
-//changes animate to still
-else
-{
-    $(this).attr("src", $(this).attr("data-still"));
-    //change to still
-    $(this).attr("data-state", "still");
-}
+    {
+        $(this).attr("src", $(this).attr("data-animate"));
+        //change to animate
+        $(this).attr("data-state", "animate");
+    }
+    //changes animate to still
+    else
+    {
+        $(this).attr("src", $(this).attr("data-still"));
+        //change to still
+        $(this).attr("data-state", "still");
+    }
 })
+
+function resetButton(){
+    $("#buttonsHome").empty();
+
+}
 
 
 
